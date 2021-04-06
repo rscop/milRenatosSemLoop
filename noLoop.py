ce12e73e7e2de0f0ebaeb0e829e13d357d308831
Renato Scop <r.scop@hotmail.com>
2021-04-06 10:02:14 -0300

    adjust for dynamic names

--- noLoop.py
+++ noLoop.py
@@ -1,13 +1,14 @@
 class repeatString:
     def repeatString(self, oldString):
-        self.newString = oldString[0:10].replace(oldString[0], oldString[0:10])
-        self.newString = self.newString.replace(self.newString[0], self.newString[0:10])
+        self.newString = 'XXXXXXXXXX'
+        self.newString = self.newString.replace(self.newString[0], self.newString)
+        # self.newString = self.newString.replace(self.newString[0], self.newString)
 
     def changeName(self, name):
         self.newString = self.newString.replace(self.newString[0], name)
 
-    def fixEndString(self):
-        self.newString = self.newString[:-10] + ' e Renato.'
+    def fixEndString(self, name):
+        self.newString = self.newString[:-(len(name)+4)] + ' e ' + name + '.'
 
     def getNewString(self):
         return self.newString
@@ -17,13 +18,14 @@ class milRenatosComLoop:
         self.loopDont = self.noLoop(StringToReplace)
 
     def noLoop(self, StringToReplace):
+        actualName = StringToReplace + ', '
         string = repeatString()
-        string.repeatString(StringToReplace)
-        string.changeName(StringToReplace[10:])
-        string.fixEndString()
+        string.repeatString(actualName)
+        string.changeName(actualName)
+        string.fixEndString(StringToReplace)
 
         print(string.getNewString())
 
 if __name__ == '__main__':
-    withoutLoop = milRenatosComLoop('XXXXXXXXXXRenato, ')
+    withoutLoop = milRenatosComLoop('Renato')
     withoutLoop.loopDont
